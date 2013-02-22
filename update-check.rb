69f19a8752a479fe3e9e2176db7b3ba59dcc5d07
Stephen Connolly <stephen.alan.connolly@gmail.com>
2013-02-22 12:04:01 +0000

    Update update-check

--- update-check.rb
+++ update-check.rb
@@ -6,10 +6,13 @@ require 'open-uri'
 
 current = Gem::Version.new((Nokogiri::HTML(open('http://knockoutjs.com/index.html')).css('a[class="download-button"]').first['href'].match /.*knockout-(.*)\.js/).captures[0])
 
-maven = Gem::Version.new(Nokogiri::XML(File.open("pom.xml")).remove_namespaces!.xpath("/project/version").first.content().sub /-SNAPSHOT/,"")
+maven = Gem::Version.new(Nokogiri::XML(File.open(File.expand_path('../pom.xml', __FILE__))).remove_namespaces!.xpath("/project/version").first.content().sub /-SNAPSHOT/,"")
+
+puts "["+File.dirname(__FILE__)+"]   Next redist: " + maven.to_s
+puts "["+File.dirname(__FILE__)+"] Last upstream: " + current.to_s
 
 if current >= maven
-puts "A release can be made"
-exit 1 
+	puts "["+File.dirname(__FILE__)+"] A release can be made!!!"
+	exit 1 
 end
-puts "Everything up to date"
+puts "["+File.dirname(__FILE__)+"] Everything up to date."
